a575b9b0a98a9f84f40fb17f5e272985130e1ad8
absencee_ <ilyasofli06@gmail.com>
2025-01-23 13:48:24 +0300

    feat: added Game
    -> Game is a static class which will help us.

---
+++ Assets/_Scripts/Game/Game.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+namespace com.game
+{
+    public static class Game
+    {
+        public static bool Paused { get; private set; }
+        public static bool Initialized { get; set; }
+
+        public static event Action OnPause = null;
+        public static event Action OnResume = null;
+
+        public static void Pause()
+        {
+            if (Paused) return;
+
+            Time.timeScale = 0f;
+            Paused = true;
+
+            OnPause?.Invoke();
+        }
+
+        public static void Resume(float overrideTimeScale = 1f)
+        {
+            if (!Paused) return;
+
+            Time.timeScale = overrideTimeScale;
+            Paused = false;
+
+            OnResume?.Invoke();
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+        static void Reset()
+        {
+            Initialized = false;
+            Paused = true;
+            OnPause = null;
+            OnResume = null;
+
+            Resume();
+        }
+    }
+}
