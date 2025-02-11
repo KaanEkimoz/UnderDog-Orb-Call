e62384246ff211d3669e0d317f5c3ce34ab641cc
absencee_ <ilyasofli06@gmail.com>
2025-02-11 21:12:59 +0300

    refactor: player light

--- Assets/_Scripts/Player/PlayerLight.cs
+++ Assets/_Scripts/Player/PlayerLight.cs
@@ -1,4 +1,3 @@
-using com.absence.attributes.experimental;
 using com.game.player.statsystemextensions;
 using com.game.testing;
 using UnityEngine;
@@ -7,28 +6,34 @@ namespace com.game.player
 {
     public class PlayerLight : MonoBehaviour
     {
-        static float s_threshold = 0.1f;
+        [System.Serializable]
+        public class FieldSettings
+        {
+            [Min(0)] public float InitialValue;
+            [Min(0)] public float LightStatCoefficient;
+            [Min(0)] public float StepAmount;
+            [Min(0)] public float AnimationSpeed;
+        }
+
+        const float k_threshold = 0.1f;
 
         [SerializeField] private Light m_light;
+        [SerializeField] private LayerMask m_groundMask;
         [SerializeField] private float m_generalLightStatCoefficient;
+        [SerializeField] private float m_threshold = k_threshold;
 
-        [SerializeField, BeginFoldoutGroup("Inner Spot Angle Settings"), Range(0f, 180f)] private float m_zeroInnerSpot;
-        [SerializeField] private float m_innerSpotLightStatCoefficient;
-        [SerializeField] private float m_stepInnerSpot;
-        [SerializeField, EndFoldoutGroup] private float m_innerSpotSpeed;
-
-        [SerializeField, BeginFoldoutGroup("Intensity Settings")] private float m_zeroIntensity;
-        [SerializeField] private float m_intensityLigthStatCoefficient;
-        [SerializeField] private float m_stepIntensity;
-        [SerializeField, EndFoldoutGroup] private float m_intensitySpeed;
-
-        [SerializeField, BeginFoldoutGroup("Range Settings")] private float m_zeroRange;
-        [SerializeField] private float m_rangeLightStatCoefficient;
-        [SerializeField] private float m_stepRange;
-        [SerializeField, EndFoldoutGroup] private float m_rangeSpeed;
+        [SerializeField] private FieldSettings m_innerSpotAngleSettings;
+        [SerializeField] private FieldSettings m_outerSpotAngleSettings;
+        [SerializeField] private FieldSettings m_intensitySettings;
+        [SerializeField] private FieldSettings m_rangeSettings;
+        [SerializeField] private FieldSettings m_localHeightSettings;
 
         PlayerStats m_playerStats;
         PlayerOrbHandler_Test m_orbHandler;
+        int m_orbsInHand;
+        float m_rawLightStat;
+        float m_generalCoefficient;
+        float m_generalShift;
 
         private void Start()
         {
@@ -38,33 +43,81 @@ namespace com.game.player
 
         private void Update()
         {
-            int orbsInHand = m_orbHandler.OrbsInHand;
+            CacheVariables();
 
-            float refinedLightStat = m_playerStats.GetStat(PlayerStatType.LightStrength) * m_generalLightStatCoefficient;
-            float generalCoefficient = orbsInHand;
-            float generalShift = refinedLightStat;
+            m_light.innerSpotAngle = UpdateViaFieldData(m_innerSpotAngleSettings, m_light.innerSpotAngle);
+            m_light.spotAngle = UpdateViaFieldData(m_outerSpotAngleSettings, m_light.spotAngle);
+            m_light.intensity = UpdateViaFieldData(m_intensitySettings, m_light.intensity);
+            m_light.range = UpdateViaFieldData(m_rangeSettings, m_light.range);
 
-            float innerSpotShift = m_innerSpotLightStatCoefficient * generalShift;
-            float rangeShift = m_rangeLightStatCoefficient * generalShift;
-            float intensityShift = m_intensityLigthStatCoefficient * generalShift;
+            Vector3 lightLocalPosition = m_light.transform.localPosition;
+            lightLocalPosition.y = UpdateViaFieldData(m_localHeightSettings, lightLocalPosition.y);
+            m_light.transform.localPosition = lightLocalPosition;
+        }
 
-            float initialInnerSpot = m_light.innerSpotAngle;
-            float targetInnerSpot = (generalCoefficient * m_stepInnerSpot) + innerSpotShift;
-            targetInnerSpot += m_zeroInnerSpot;
+        void CacheVariables()
+        {
+            m_orbsInHand = m_orbHandler.OrbsInHand;
+            m_rawLightStat = m_playerStats.GetStat(PlayerStatType.LightStrength);
 
-            targetInnerSpot = Mathf.Clamp(targetInnerSpot, 0f, 180f);
+            float refinedLightStat = m_rawLightStat * m_generalLightStatCoefficient;
+            m_generalCoefficient = m_orbsInHand;
+            m_generalShift = refinedLightStat;
+        }
 
-            //m_light.colorTemperature = 
+        float UpdateViaFieldData(FieldSettings target, float initialValue)
+        {
+            float result = initialValue;
 
-            float initialRange = m_light.range;
-            float initialIntensity = m_light.intensity;
+            float localZero = target.InitialValue;
+            float localCoefficient = target.LightStatCoefficient;
+            float localSpeed = target.AnimationSpeed;
+            float localStep = target.StepAmount;
 
-            float targetRange = m_zeroRange + (generalCoefficient * m_stepRange) + rangeShift;
-            float targetIntensity = m_zeroIntensity + (generalCoefficient * m_stepIntensity) + intensityShift;
+            float shift = localCoefficient * m_generalShift;
+            float targetValue = (m_generalCoefficient * localStep) + shift + localZero;
 
-            if (Mathf.Abs(initialIntensity - targetIntensity) > s_threshold) m_light.range = Mathf.Lerp(initialRange, targetRange, m_rangeSpeed * Time.deltaTime);
-            if (Mathf.Abs(initialRange - targetRange) > s_threshold) m_light.intensity = Mathf.Lerp(initialIntensity, targetIntensity, m_intensitySpeed * Time.deltaTime);
-            if (Mathf.Abs(initialInnerSpot - targetInnerSpot) > s_threshold) m_light.innerSpotAngle = Mathf.Lerp(initialInnerSpot, targetInnerSpot, m_innerSpotSpeed * Time.deltaTime);
+            if (Mathf.Abs(initialValue - targetValue) > m_threshold) 
+                result = Mathf.Lerp(initialValue, targetValue, localSpeed * Time.deltaTime);
+
+            return result;
+        }
+
+        private void OnDrawGizmos()
+        {
+            float outerAngleInRads = m_light.spotAngle * Mathf.Deg2Rad;
+            float innerAngleInRads = m_light.innerSpotAngle * Mathf.Deg2Rad;
+            float range = m_light.range;
+
+            bool hit = Physics.Raycast(m_light.transform.position,
+            Vector3.down,
+            out RaycastHit groundData,
+            range,
+            m_groundMask,
+            QueryTriggerInteraction.UseGlobal);
+
+            float innerRadius = Mathf.Tan(innerAngleInRads / 2) * groundData.distance;
+            float outerRadius = Mathf.Tan(outerAngleInRads / 2) * groundData.distance;
+
+            const float kAlpha = 1f;
+
+            Color fullSeenColor = Color.red;
+            fullSeenColor.a = kAlpha;
+
+            Color halfSeenColor = Color.yellow;
+            halfSeenColor.a = kAlpha;
+
+#if UNITY_EDITOR
+            UnityEditor.Handles.color = fullSeenColor;
+            if (hit) UnityEditor.Handles.DrawWireDisc(groundData.point, Vector2.up, innerRadius);
+            UnityEditor.Handles.color = halfSeenColor;
+            if (hit) UnityEditor.Handles.DrawWireDisc(groundData.point, Vector2.up, outerRadius);
+#endif
+
+            //Gizmos.color = fullSeenColor;
+            //if (hit) Gizmos.DrawSphere(groundData.point, innerRadius);
+            //Gizmos.color = halfSeenColor;
+            //if (hit) Gizmos.DrawSphere(groundData.point, outerRadius);
         }
     }
 }
